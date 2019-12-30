8176b3af9b47a390a1360a755c5717c0007f80dd
agwiacek <agwiacek@pgs-soft.com>
2019-12-30 14:17:08 +0100

    csv file: read, pick, print winners

--- participants.py
+++ participants.py
@@ -1,25 +1,33 @@
 import csv
-#import json
 import random
 
-with open('data/participants1.csv') as csvfile:
-    readCsv = csv.reader(csvfile, delimiter=',')
-    for row in readCsv:
-        print(row[1], row[2])
+
+def load_participants_csv(file_name):
+    with open(file_name) as csv_file:
+        # participants_csv = [{data: names for data, names in row.items()}
+        #                    for row in csv.DictReader(csv_file, delimiter=',')]
+        reader = csv.DictReader(csv_file, delimiter=',')
+        participants_list = []
+        for row in reader:
+            participants_list.append(row)
+        return participants_list
 
 
-participants = open('data/participants1.csv', 'r').readlines()
-winners = random.sample(participants, 3)
-print('The winners are:', winners)
+# return participants_csv
 
 
+def pick_winners(number, participants):
+    winners_csv = random.sample(participants, number)
+    return winners_csv
 
 
-#def draw_the_winners():
+def print_winners(winners_csv):
+    print('The winners are: ')
+    for name in winners_csv:
+        print(name['first_name'], name['last_name'])
 
-#    draw = random.choice(open('data/participants1.csv', 'r').readlines())
-#    print(draw)
 
-#with open('data/participants1.json') as jsonfile:
-#    readJson = json.load(jsonfile)
-#    print(readJson)
+if __name__ == '__main__':
+    participants = load_participants_csv('data/participants1.csv')
+    # pick_winners(5, participants)
+    print_winners(pick_winners(3, participants))
