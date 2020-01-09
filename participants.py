101f17aa515dd621adaf41aa809f502d4ab56f5a
agwiacek <agwiacek@pgs-soft.com>
2020-01-09 14:19:38 +0100

    pathlib; sprint 1

--- participants.py
+++ participants.py
@@ -1,33 +1,54 @@
 import csv
+import json
 import random
+from pathlib import Path
+
+
+class Participant:
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+def get_file_path(file_name, file_extension):
+    data_path = Path('data/').joinpath(file_name + '.' + file_extension)
+    return data_path
 
 
 def load_participants_csv(file_name):
     with open(file_name) as csv_file:
-        # participants_csv = [{data: names for data, names in row.items()}
-        #                    for row in csv.DictReader(csv_file, delimiter=',')]
         reader = csv.DictReader(csv_file, delimiter=',')
         participants_list = []
         for row in reader:
-            participants_list.append(row)
+            participant = Participant(row['id'], row['first_name'], row['last_name'])
+            participants_list.append(participant)
         return participants_list
 
 
-# return participants_csv
+def load_participants_json(file_name):
+    with open(file_name) as json_file:
+        participants_json = json.load(json_file)
+        participants_list = []
+        for row in participants_json:
+            participant = Participant(row['id'], row['first_name'], row['last_name'])
+            participants_list.append(participant)
+        return participants_list
 
 
-def pick_winners(number, participants):
-    winners_csv = random.sample(participants, number)
-    return winners_csv
+def pick_winners(participants, number):
+    winners = random.sample(participants, number)
+    return winners
 
 
-def print_winners(winners_csv):
+def print_winners(winners):
     print('The winners are: ')
-    for name in winners_csv:
-        print(name['first_name'], name['last_name'])
+    for name in winners:
+        print(name.first_name, name.last_name)
 
 
 if __name__ == '__main__':
-    participants = load_participants_csv('data/participants1.csv')
-    # pick_winners(5, participants)
-    print_winners(pick_winners(3, participants))
+    participants = load_participants_csv(get_file_path('participants1', 'csv'))
+    json = load_participants_json(get_file_path('participants1', 'json'))
+    print_winners(pick_winners(participants, 3))
+    print_winners(pick_winners(json, 3))
